9a92f8882b44ef3acd3ead30ffe6ea3dbabc07f2
Henrique Herbs Rosa <henriquehhr@gmail.com>
2022-05-11 01:08:06 -0300

    feature: added signIn signUp pages

---
+++ src/components/pages/SignUpPage/SignUpPage.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+import { $Form, $Button, $Input, $Main } from "../../../globalStyles/globalStyles.js";
+
+export default function SignUpPage() {
+
+    const [disabled, setDisabled] = useState(false);
+    const [singinInfo, setSinginInfo] = useState({
+        email: "",
+        name: "",
+        password: ""
+    });
+    const navigate = useNavigate();
+
+    function sendSinginInfo(e) {
+        e.preventDefault();
+        setDisabled(true);
+        const url = "http://localhost:5000/signup";
+        const promisse = axios.post(url, singinInfo);
+        promisse.then(() => navigate("/"));
+        promisse.catch(() => {
+            setDisabled(false);
+            alert("Erro no cadastro. Tente novamente.");
+        });
+    }
+
+    return (
+        <$Main>
+            <h1>Driven-books</h1>
+            <$Form onSubmit={sendSinginInfo} action="">
+                <$Input
+                    placeholder="email"
+                    type="email"
+                    value={singinInfo.email}
+                    onChange={e => setSinginInfo({ ...singinInfo, email: e.target.value })}
+                    disabled={disabled}
+                />
+                <$Input
+                    placeholder="senha"
+                    type="password"
+                    value={singinInfo.password}
+                    onChange={e => setSinginInfo({ ...singinInfo, password: e.target.value })}
+                    disabled={disabled}
+                />
+                <$Input
+                    placeholder="nome"
+                    type="text"
+                    value={singinInfo.name}
+                    onChange={e => setSinginInfo({ ...singinInfo, name: e.target.value })}
+                    disabled={disabled}
+                />
+                <$Button type="submit" className="big" disabled={disabled}>Cadastrar
+                </$Button>
+            </$Form>
+            <Link to="/">
+                <p>Já tem uma conta? Faça login!</p>
+            </Link>
+        </$Main>
+    );
+}
